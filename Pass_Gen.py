931315d2f900859a2afae4c3e14b73fc8c84a09f
AceEagle <50935463+AceEagle@users.noreply.github.com>
2020-05-09 00:05:46 -0400

    Files

---
+++ Pass_Gen.py
@@ -0,0 +1,24 @@
+import random
+from PyQt5.QtWidgets import *
+
+def PassGen(qte, symbs):
+    Pass = 0
+    numb = '9'
+    bnumb = 10
+    if qte < 1:
+        raise TypeError
+
+    elif qte < 2:
+        Pass = random.randrange(0,9)
+        return Pass
+
+    else:
+        RealNumb = float(numb * qte)
+        BeforeNumb = '0'
+        RealBeforeNumb = float('1' + BeforeNumb * (qte-1))
+        Pass = random.randrange(RealBeforeNumb, RealNumb)
+        return Pass
+
+print(PassGen(6, 1))
+
+app = QApplication([])
