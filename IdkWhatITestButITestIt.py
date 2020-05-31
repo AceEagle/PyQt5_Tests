6590775cba3691ba9fd240850d2d3819eb26e3a5
Mathieu <mathieu.vigneault.19@gmail.com>
2020-05-31 13:14:11 -0400

    end of idkwhatitest

--- IdkWhatITestButITestIt.py
+++ IdkWhatITestButITestIt.py
@@ -1,3 +1,33 @@
 import sys
 import random
-from PySide2 import QtCore,
+from PySide2 import QtCore,QtWidgets, QtGui
+
+
+class MyTest(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Bien le Bonjour"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyTest()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
